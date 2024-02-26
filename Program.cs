f03fd7157826e43e4e4b55d69033ef74f3efc0dd
ramon <ramonintouss@gmail.com>
2024-02-26 19:10:59 -0300

    commit 4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 using Microsoft.Data.Sqlite;
 using System.Data;
 using ProjetoLoja.Repository;
+using ProjetoLoja.DataBase.MyDbContext;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
 
 builder.Configuration.AddJsonFile("appsettings.json");
 
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddTransient<IDbConnection>((sp) => new SqliteConnection(connectionString));
-builder.Services.AddTransient<DapperRepository>();
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
 
---
+++ Repository/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace ProjetoLoja.Repository
+{
+    public class BaseRepository
+    {
+        private readonly string connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected IDbConnection connection 
+        { 
+            get 
+            { 
+                return new SqliteConnection(connectionString); 
+            } 
+        }
+    }
+}
